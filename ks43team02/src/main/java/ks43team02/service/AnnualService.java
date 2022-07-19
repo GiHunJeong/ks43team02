c8cca0ba6a413d0102dd3f722e674cde2e5aa6d3
beckminju <smileworl08@naver.com>
2022-07-19 16:57:41 +0900

    페이지 넘어갈때 스크립트 충돌되어 수정했습니다.

--- ks43team02/src/main/java/ks43team02/service/AnnualService.java
+++ ks43team02/src/main/java/ks43team02/service/AnnualService.java
@@ -17,61 +17,61 @@ import ks43team02.mapper.AnnualSettingMapper;
 @Service
 @Transactional
 public class AnnualService {
-	/*
-	 * 의존성 주입
-	 * */
-	private final AnnualMapper annualMapper;
-	private final AnnualSettingMapper annualSettingMapper;
-	
-	public AnnualService(AnnualMapper annualMapper, AnnualSettingMapper annualSettingMapper) {
-		this.annualMapper = annualMapper;
-		this.annualSettingMapper = annualSettingMapper;
-	}
-	
-	//연차등록 가져오기 select
-	public List<AnnualApplication> getAnnualApplication() {
-		List<AnnualApplication> annualApplication = annualMapper.getAnnualApplication();
-		return annualApplication;
-	}
-	
-	
-	//연차등록 폼 insert
-	public int addAnnualApplication(AnnualApplication annualApplication) {
-		int result = annualMapper.addAnnualApplication(annualApplication);
-		return result;
-	}
-	
-	//등록된 연차 리스트 select
-	public List<AnnualList> getAnnualList(){
-		List<AnnualList> annualList = annualMapper.getAnnualList();
-		return annualList;
-	}
-	
-	//등록된 연차 리스트 
-	public int getAnnualList(AnnualList annualList) {
-		int result = annualMapper.getAnnualList(annualList);
-		return result;
-	}
+   /*
+    * 의존성 주입
+    * */
+   private final AnnualMapper annualMapper;
+   private final AnnualSettingMapper annualSettingMapper;
+   
+   public AnnualService(AnnualMapper annualMapper, AnnualSettingMapper annualSettingMapper) {
+      this.annualMapper = annualMapper;
+      this.annualSettingMapper = annualSettingMapper;
+   }
+   
+   //연차등록 가져오기 select
+   public List<AnnualApplication> getAnnualApplication() {
+      List<AnnualApplication> annualApplication = annualMapper.getAnnualApplication();
+      return annualApplication;
+   }
+   
+   
+   //연차등록 폼 insert
+   public int addAnnualApplication(AnnualApplication annualApplication) {
+      int result = annualMapper.addAnnualApplication(annualApplication);
+      return result;
+   }
+   
+   //등록된 연차 리스트 select
+   public List<AnnualList> getAnnualList(){
+      List<AnnualList> annualList = annualMapper.getAnnualList();
+      return annualList;
+   }
+   
+   //등록된 연차 리스트 
+   public int getAnnualList(AnnualList annualList) {
+      int result = annualMapper.getAnnualList(annualList);
+      return result;
+   }
 
 
-	//연차세팅 가져오기
-	public List<AnnualSetting> getAnnualSetting(){ 
-		List<AnnualSetting> annualSetting = annualSettingMapper.getAnnualSetting(); 
-		return annualSetting;
-	}
+   //연차세팅 가져오기
+   public List<AnnualSetting> getAnnualSetting(){ 
+      List<AnnualSetting> annualSetting = annualSettingMapper.getAnnualSetting(); 
+      return annualSetting;
+   }
 
-	//연차기준 가져오기
-	public List<AnnualStandard> getAnnualStandard(){
-		List<AnnualStandard> annualStandard = annualMapper.getAnnualStandard();
-		return annualStandard;
-	}
+   //연차기준 가져오기
+   public List<AnnualStandard> getAnnualStandard(){
+      List<AnnualStandard> annualStandard = annualMapper.getAnnualStandard();
+      return annualStandard;
+   }
 
-	//연차카테고리 가져오기
-	public List<CateAnnual> getCateAnnual(){ 
-		List<CateAnnual> cateAnnual = annualMapper.getCateAnnual();
-		return cateAnnual; 
-	}
-	
-	 
+   //연차카테고리 가져오기
+   public List<CateAnnual> getCateAnnual(){ 
+      List<CateAnnual> cateAnnual = annualMapper.getCateAnnual();
+      return cateAnnual; 
+   }
+   
+    
  
 }
