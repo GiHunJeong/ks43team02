28305503829ab8a57cc27aba2fecea1eea32f430
johnn <johnn@192.168.31.132>
2022-07-20 11:37:33 +0900

    Revert "ensecretary 2022-07-20 11:31"
    This reverts commit f67513200e51e31c140a4e44cc05a492853a1e9d.

--- ks43team02/src/main/java/ks43team02/service/PaymentService.java
+++ ks43team02/src/main/java/ks43team02/service/PaymentService.java
@@ -1,10 +1,5 @@
 package ks43team02.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
 public class PaymentService {
 
 }
