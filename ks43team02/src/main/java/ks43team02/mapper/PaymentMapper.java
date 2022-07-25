a11d35a868ca4fc1ce508d71a456ab2c0d63ce52
johnn <johnn@192.168.31.132>
2022-07-25 10:47:21 +0900

    ensecretary 2022-07-25 10:45
    DocumentCateS.java
    payment_request.html
    PaymentController.java
    PaymentMapper.java
    kPaymentMapper.xml
    PaymentService.java

--- ks43team02/src/main/java/ks43team02/mapper/PaymentMapper.java
+++ ks43team02/src/main/java/ks43team02/mapper/PaymentMapper.java
@@ -1,8 +1,13 @@
 package ks43team02.mapper;
 
+import java.util.*;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import ks43team02.dto.DocumentCateS;
+
 @Mapper
-public class PaymentMapper {
-	
+public interface PaymentMapper {
+	//문서 카테고리소 조회
+	public List<DocumentCateS> getDocumentCateSList();
 }
--- ks43team02/src/main/java/ks43team02/service/PaymentService.java
+++ ks43team02/src/main/java/ks43team02/service/PaymentService.java
@@ -1,10 +1,25 @@
 package ks43team02.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import ks43team02.dto.DocumentCateS;
+import ks43team02.mapper.PaymentMapper;
 
 @Service
 @Transactional
 public class PaymentService {
-
+	//의존성 주입
+	private final PaymentMapper paymentMapper;
+	
+	public PaymentService(PaymentMapper paymentMapper) {
+		this.paymentMapper = paymentMapper;
+	}
+	
+	public List<DocumentCateS> getDocumentCateSList() {
+		List<DocumentCateS> documentCateSList = paymentMapper.getDocumentCateSList();
+		return documentCateSList;
+	}
 }
