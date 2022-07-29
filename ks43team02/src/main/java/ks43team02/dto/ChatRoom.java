5d2ddeae89e5ce87134dc3f6568c60ca688afc89
ksmartKim <dagon8361@naver.com>
2022-07-29 14:22:48 +0900

    220729 채팅방 수정

--- ks43team02/src/main/java/ks43team02/dto/ChatRoom.java
+++ ks43team02/src/main/java/ks43team02/dto/ChatRoom.java
@@ -1,9 +1,34 @@
 package ks43team02.dto;
 
+import java.time.LocalDateTime;
+
 public class ChatRoom {
+	private String messengerChatroomCode;
+	private String cpRepresentativeCode;
+	private String superAdminId;
 	private int roomNumber;
 	private String roomName;
+	private LocalDateTime chatroomRegDate;
+	private LocalDateTime chatroomEndDate;
 	
+	public String getMessengerChatroomCode() {
+		return messengerChatroomCode;
+	}
+	public void setMessengerChatroomCode(String messengerChatroomCode) {
+		this.messengerChatroomCode = messengerChatroomCode;
+	}
+	public String getCpRepresentativeCode() {
+		return cpRepresentativeCode;
+	}
+	public void setCpRepresentativeCode(String cpRepresentativeCode) {
+		this.cpRepresentativeCode = cpRepresentativeCode;
+	}
+	public String getSuperAdminId() {
+		return superAdminId;
+	}
+	public void setSuperAdminId(String superAdminId) {
+		this.superAdminId = superAdminId;
+	}
 	public int getRoomNumber() {
 		return roomNumber;
 	}
@@ -16,10 +41,23 @@ public class ChatRoom {
 	public void setRoomName(String roomName) {
 		this.roomName = roomName;
 	}
-	
+	public LocalDateTime getChatroomRegDate() {
+		return chatroomRegDate;
+	}
+	public void setChatroomRegDate(LocalDateTime chatroomRegDate) {
+		this.chatroomRegDate = chatroomRegDate;
+	}
+	public LocalDateTime getChatroomEndDate() {
+		return chatroomEndDate;
+	}
+	public void setChatroomEndDate(LocalDateTime chatroomEndDate) {
+		this.chatroomEndDate = chatroomEndDate;
+	}
 	@Override
 	public String toString() {
-		return "ChatRoom [roomNumber=" + roomNumber + ", roomName=" + roomName + "]";
+		return "ChatRoom [messengerChatroomCode=" + messengerChatroomCode + ", cpRepresentativeCode="
+				+ cpRepresentativeCode + ", superAdminId=" + superAdminId + ", roomNumber=" + roomNumber + ", roomName="
+				+ roomName + ", chatroomRegDate=" + chatroomRegDate + ", chatroomEndDate=" + chatroomEndDate + "]";
 	}
 	
 }
