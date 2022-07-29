e98c003747c41b3fe8d4f6992244b216b0a696d3
johnn <johnn@192.168.31.132>
2022-07-29 14:07:13 +0900

    ensecretary 2022-07-29 14:06
    ks43team02/src/main/java/ks43team02/dto/ApprovalProgress.java
    ks43team02/src/main/java/ks43team02/dto/ApprovalRequest.java 빠진 부분 추가
    ks43team02/src/main/resources/templates/fragments/header.html 로그인 부분 수정
    ks43team02/src/main/java/ks43team02/controller/LoginController.java 필요없는
    부분 삭제

--- ks43team02/src/main/java/ks43team02/dto/ApprovalProgress.java
+++ ks43team02/src/main/java/ks43team02/dto/ApprovalProgress.java
@@ -10,7 +10,6 @@ public class ApprovalProgress {
 	private int approverRank;
 	private String documentCateSCode;
 	private String emplyName;
-
 	private int currentApprovalStep;
 	private String returnStatus;
 	private String comment;
@@ -43,223 +42,86 @@ public class ApprovalProgress {
 	public void setEmply(Emply emply) {
 		this.emply = emply;
 	}
-
-	private String currentApprovalStep;
-	private String returnStauts;
-	private String comment;
-	private String regDate;
-	
-	private ApprovalRequest approvalRequest;
-	private DocumentCateS documentCateS;
-	private Emply emply;
-	
-
 	public String getApprovalProgressCode() {
 		return approvalProgressCode;
 	}
-
-
-
 	public void setApprovalProgressCode(String approvalProgressCode) {
 		this.approvalProgressCode = approvalProgressCode;
 	}
-
-
-
 	public String getCpRepresentativeCode() {
 		return cpRepresentativeCode;
 	}
-
-
-
 	public void setCpRepresentativeCode(String cpRepresentativeCode) {
 		this.cpRepresentativeCode = cpRepresentativeCode;
 	}
-
-
-
 	public String getSuperAdminId() {
 		return superAdminId;
 	}
-
-
-
 	public void setSuperAdminId(String superAdminId) {
 		this.superAdminId = superAdminId;
 	}
-
-
-
 	public String getApprovalRequestCode() {
 		return approvalRequestCode;
 	}
-
-
-
 	public void setApprovalRequestCode(String approvalRequestCode) {
 		this.approvalRequestCode = approvalRequestCode;
 	}
-
-
-
 	public String getEmplyId() {
 		return emplyId;
 	}
-
-
-
 	public void setEmplyId(String emplyId) {
 		this.emplyId = emplyId;
 	}
-
-
-
 	public String getApproverId() {
 		return approverId;
 	}
-
-
-
 	public void setApproverId(String approverId) {
 		this.approverId = approverId;
 	}
-
-
-
 	public String getDocumentCateSCode() {
 		return documentCateSCode;
 	}
-
-
-
 	public void setDocumentCateSCode(String documentCateSCode) {
 		this.documentCateSCode = documentCateSCode;
 	}
-
-
-
 	public String getEmplyName() {
 		return emplyName;
 	}
-
-
-
 	public void setEmplyName(String emplyName) {
 		this.emplyName = emplyName;
 	}
-
 	public int getCurrentApprovalStep() {
 		return currentApprovalStep;
 	}
 	public void setCurrentApprovalStep(int currentApprovalStep) {
-
-
-
-
-	public String getCurrentApprovalStep() {
-		return currentApprovalStep;
-	}
-
-
-
-	public void setCurrentApprovalStep(String currentApprovalStep) {
-
 		this.currentApprovalStep = currentApprovalStep;
 	}
-
-
-
-	public String getReturnStauts() {
-		return returnStauts;
+	public String getReturnStatus() {
+		return returnStatus;
 	}
-
-
-
-	public void setReturnStauts(String returnStauts) {
-		this.returnStauts = returnStauts;
+	public void setReturnStatus(String returnStatus) {
+		this.returnStatus = returnStatus;
 	}
-
-
-
 	public String getComment() {
 		return comment;
 	}
-
-
-
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
-
-
 	public String getRegDate() {
 		return regDate;
 	}
-
-
-
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
-
-
-
-	public ApprovalRequest getApprovalRequest() {
-		return approvalRequest;
-	}
-
-
-
-	public void setApprovalRequest(ApprovalRequest approvalRequest) {
-		this.approvalRequest = approvalRequest;
-	}
-
-
-
-	public DocumentCateS getDocumentCateS() {
-		return documentCateS;
-	}
-
-
-
-	public void setDocumentCateS(DocumentCateS documentCateS) {
-		this.documentCateS = documentCateS;
-	}
-
-
-
-	public Emply getEmply() {
-		return emply;
-	}
-
-
-
-	public void setEmply(Emply emply) {
-		this.emply = emply;
-	}
-
-
-
 	@Override
 	public String toString() {
 		return "ApprovalProgress [approvalProgressCode=" + approvalProgressCode + ", cpRepresentativeCode="
 				+ cpRepresentativeCode + ", superAdminId=" + superAdminId + ", approvalRequestCode="
-
 				+ approvalRequestCode + ", emplyId=" + emplyId + ", approverId=" + approverId + ", approverRank="
 				+ approverRank + ", documentCateSCode=" + documentCateSCode + ", emplyName=" + emplyName
 				+ ", currentApprovalStep=" + currentApprovalStep + ", returnStatus=" + returnStatus + ", comment="
 				+ comment + ", regDate=" + regDate + ", approvalRequest=" + approvalRequest + ", documentCateS="
 				+ documentCateS + ", emply=" + emply + "]";
 	}
-
-				+ approvalRequestCode + ", emplyId=" + emplyId + ", approverId=" + approverId + ", documentCateSCode="
-				+ documentCateSCode + ", emplyName=" + emplyName + ", currentApprovalStep=" + currentApprovalStep
-				+ ", returnStauts=" + returnStauts + ", comment=" + comment + ", regDate=" + regDate
-				+ ", approvalRequest=" + approvalRequest + ", documentCateS=" + documentCateS + ", emply=" + emply
-				+ "]";
-	}
-
-
-
 }
--- ks43team02/src/main/java/ks43team02/dto/ApprovalRequest.java
+++ ks43team02/src/main/java/ks43team02/dto/ApprovalRequest.java
@@ -15,48 +15,44 @@ public class ApprovalRequest {
 	private String applicationFormGroupNameCode;
 	private String regDate;
 	
-	private DocumentCateS documentCateS;
-
+	DocumentCateS documentCateS;
+	
+	public DocumentCateS getDocumentCateS() {
+		return documentCateS;
+	}
+	public void setDocumentCateS(DocumentCateS documentCateS) {
+		this.documentCateS = documentCateS;
+	}
 	public String getApprovalRequestCode() {
 		return approvalRequestCode;
 	}
-
 	public void setApprovalRequestCode(String approvalRequestCode) {
 		this.approvalRequestCode = approvalRequestCode;
 	}
-
 	public String getCpRepresentativeCode() {
 		return cpRepresentativeCode;
 	}
-
 	public void setCpRepresentativeCode(String cpRepresentativeCode) {
 		this.cpRepresentativeCode = cpRepresentativeCode;
 	}
-
 	public String getSuperAdminId() {
 		return superAdminId;
 	}
-
 	public void setSuperAdminId(String superAdminId) {
 		this.superAdminId = superAdminId;
 	}
-
 	public String getEmplyId() {
 		return emplyId;
 	}
-
 	public void setEmplyId(String emplyId) {
 		this.emplyId = emplyId;
 	}
-
 	public String getDocumentCateSCode() {
 		return documentCateSCode;
 	}
-
 	public void setDocumentCateSCode(String documentCateSCode) {
 		this.documentCateSCode = documentCateSCode;
 	}
-
 	public int getApprovalProgressStep() {
 		return approvalProgressStep;
 	}
@@ -67,81 +63,44 @@ public class ApprovalRequest {
 		return totalApprovalStep;
 	}
 	public void setTotalApprovalStep(int totalApprovalStep) {
-
-
-	public String getApprovalProgressStep() {
-		return approvalProgressStep;
-	}
-
-	public void setApprovalProgressStep(String approvalProgressStep) {
-		this.approvalProgressStep = approvalProgressStep;
-	}
-
-	public String getTotalApprovalStep() {
-		return totalApprovalStep;
-	}
-
-	public void setTotalApprovalStep(String totalApprovalStep) {
-
 		this.totalApprovalStep = totalApprovalStep;
 	}
-
 	public String getApprovalProgressStatus() {
 		return approvalProgressStatus;
 	}
-
 	public void setApprovalProgressStatus(String approvalProgressStatus) {
 		this.approvalProgressStatus = approvalProgressStatus;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getComment() {
 		return comment;
 	}
-
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
 	public String getApplicationFormGroupCode() {
 		return applicationFormGroupCode;
 	}
-
 	public void setApplicationFormGroupCode(String applicationFormGroupCode) {
 		this.applicationFormGroupCode = applicationFormGroupCode;
 	}
-
 	public String getApplicationFormGroupNameCode() {
 		return applicationFormGroupNameCode;
 	}
-
 	public void setApplicationFormGroupNameCode(String applicationFormGroupNameCode) {
 		this.applicationFormGroupNameCode = applicationFormGroupNameCode;
 	}
-
 	public String getRegDate() {
 		return regDate;
 	}
-
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
-
-	public DocumentCateS getDocumentCateS() {
-		return documentCateS;
-	}
-
-	public void setDocumentCateS(DocumentCateS documentCateS) {
-		this.documentCateS = documentCateS;
-	}
-
 	@Override
 	public String toString() {
 		return "ApprovalRequest [approvalRequestCode=" + approvalRequestCode + ", cpRepresentativeCode="
@@ -152,5 +111,5 @@ public class ApprovalRequest {
 				+ ", applicationFormGroupNameCode=" + applicationFormGroupNameCode + ", regDate=" + regDate
 				+ ", documentCateS=" + documentCateS + "]";
 	}
-
+	
 }
